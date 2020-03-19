e18cb7f057efad2a427343854751c92a6601269b
diana2341 <dianaponce2341@gmail.com>
2020-03-19 16:59:56 -0400

    methods

--- app/models/nutrition_controller.rb
+++ app/models/nutrition_controller.rb
@@ -5,7 +5,11 @@ def user_log_in
 
 end
 
-def food_info
+def food_input
     nutrition=Food.find_or_create_by(type:type, calories:calories,carbs:carbs,fat:fat)
 
+end
+
+def food_nutrition_info
+    information=NutritionalInformation.create(amount:amount, food_id:food_id, user_id:user_id)
 end
