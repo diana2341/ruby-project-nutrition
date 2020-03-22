b40d099d7a82dbc0da23de363ca3026808aa8b1a
diana2341 <dianaponce2341@gmail.com>
2020-03-21 23:28:43 -0400

    added options for user

--- app/models/cli.rb
+++ app/models/cli.rb
@@ -39,7 +39,7 @@ class CommandLineInterface
                 q.messages[:valid?]="Must be a number"
               end
 
-            @age_input= prompt.ask("Enter your weight in lbs.") do |q|
+            @weight_input= prompt.ask("Enter your weight in lbs.") do |q|
                 q.required true
                 q.validate /^[0-9]+$/
                 q.messages[:valid?]="Must be a number"
@@ -62,14 +62,14 @@ class CommandLineInterface
               puts" |LOG IN|"
                   puts "-----------"
 
-                @name_input= prompt.ask("Enter name") do |q|
+                @name_input= prompt.ask("Enter name:") do |q|
                     q.required true
                     q.validate /^[a-zA-Z\s]+$/
                     q.messages[:valid?]="Ivalid name input"
                     q.modify   :capitalize
                   end
               
-                @password_input= prompt.ask("Create a password") do |q|
+                @password_input= prompt.ask("Password:") do |q|
                     q.required true
                     q.validate /^[a-zA-Z0-9_.-]*$/
                     q.messages[:valid?]="Must not contain any Symbols"
@@ -81,6 +81,8 @@ class CommandLineInterface
                 end
                 if @make_food == "Yes"
                     create_food
+                    elsif @make_food =="No"
+                        user_options
                 end
                  
                 else 
@@ -92,25 +94,13 @@ class CommandLineInterface
                      if  @make_account == "Yes" 
                          create_account
                      else
-                        puts "ok leave"
+                        puts "Thank you for visiting us,hope you'll consider joining us soon !"
+                        
                  
                 end
             end
         end
-        # def food_diary
-        #     prompt = TTY::Prompt.new
-        #     @question= prompt.select("would you like to log in or sign up")do |option|
-        #     option.choice 'Yes'
-        #     option.choice 'No'
-        # end
-        # if @question == "Yes"
-        #     create_food
-        # else "ok"
-        # end
-
-
-        # end
-
+      
         def food_input(food_name,calories,carbs,fat)
 
             nutrition=Food.find_or_create_by(food_name:food_name, calories:calories,carbs:carbs,fat:fat)
@@ -131,34 +121,117 @@ class CommandLineInterface
                 q.validate /^(0|[1-9]\d*)(\.\d+)?$/
                 q.messages[:valid?]="Ivalid number input"
             end
-                @carbs_input= prompt.ask("How many carbs does it contain?" do |q|
+                @carbs_input= prompt.ask("How many carbs does it contain?") do |q|
                     q.required true
                     q.validate /^(0|[1-9]\d*)(\.\d+)?$/ 
                     q.messages[:valid?]="Ivalid number input"
                 end
+                
                     @fat_input= prompt.ask("How much Fat does it contain?") do |q|
                         q.required true
                         q.validate /^(0|[1-9]\d*)(\.\d+)?$/
                         q.messages[:valid?]="Ivalid number input"
                     end
                 food_input(@food_name,@calories_input,@carbs_input,@fat_input)
+                user_options
 
                 
 
               
         end
+        def exit 
+            puts"-------------------------------------------------------"
+                 puts"thank you for signing in #{@name_input}, see you soon "
+            puts"-------------------------------------------------------"
 
-        def food_nutrition_info
-            information=NutritionalInformation.create(amount:amount, food_id:food_id, user_id:user_id)
+        end
+        def user_options
+            prompt = TTY::Prompt.new
+
+
+            @user_option= prompt.select("Entry created, choose next option?")do |option|
+                option.choice 'Make another entry'
+                option.choice 'Show my history'
+                option.choice 'Update account'
+                option.choice 'Exit'
         end
 
-        def food_log_history(user)
-            history = Food.select do |food|
-                 puts "This #{food.food.name}"
-                 puts"contains #{food.calories}calories, #{food.carbs} carbs and #{food.fat} fat"
-            
+                if @user_option=='Show my history'
+                    food_log_history
+                elsif @user_option=="Make another entry"
+                    create_food
+                    elsif @user_option =='Update account'
+                        update_account
+                    elsif @user_option=="Exit"
+                        exit
+
             end
         end
 
+        def food_nutrition_info(amount,food_id,user_id)
+           
+            information=NutritionalInformation.create(amount:amount, food_id:food.id, user_id:user.id)
+        end
+
+        def update_account
+            prompt = TTY::Prompt.new
+            @update_option= prompt.select("Choose what to update")do |option|
+            option.choice 'password'
+            option.choice 'weight'
+           
+        end
+        if @update_option== 'password'
+            update_password
+        elsif @update_option=='weight'
+            update_weight
+        end
+
+    
+
+        end
+        def update_password#has to update
+
+            prompt = TTY::Prompt.new
+
+            new_password_input= prompt.ask("Enter new password:") do |q|
+                q.required true
+                q.validate /^[a-zA-Z0-9_.-]*$/
+                q.messages[:valid?]="Must not contain any Symbols"
+              end
+             new_password_input=@password_input
+             puts "***password updated please sign in again***"
+             user_log_in
+
+        end
+
+        def update_weight#has to update
+            prompt = TTY::Prompt.new
+
+            new_weight_input= prompt.ask("Update you weight:") do |q|
+                q.required true
+                q.validate /^(0|[1-9]\d*)(\.\d+)?$/
+                q.messages[:valid?]="Must be a number"
+              end
+             new_weight_input=@weight_input
+             puts "***Weight updated, choose another option***"
+             user_options
+            
+        
+        end
+       
+
+      
+
+        def food_log_history#(user)#shows history of specofic person
+                # user=create_account
+            history = Food.all.map {|info|info}
+            history.map do |info|
+                 puts "This #{info.food_name}"
+                 puts"contains #{info.calories} calories, #{info.carbs} grams of carbs and #{info.fat} grams of fat"
+            
+            end
+        end
+        #total up calorie count
+       #find a way to destroy entries after user exits
        
 end
--- run.rb
+++ run.rb
@@ -1,6 +1,7 @@
 require_relative './config/environment'
-
 cli=CommandLineInterface.new
 cli.signin_or_sign_up
+
+
 
 
