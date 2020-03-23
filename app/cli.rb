191ac3f1e8f74ffc7d6e955c914321b62dc79aa8
diana2341 <dianaponce2341@gmail.com>
2020-03-23 12:19:57 -0400

    added user to options

--- app/cli.rb
+++ app/cli.rb
@@ -3,6 +3,7 @@ require "tty-prompt"
 require 'pry'
 #require './options.rb'
 
+
 class CommandLineInterface
 
     
@@ -52,8 +53,7 @@ class CommandLineInterface
                 q.messages[:valid?]="Must not contain any Symbols"
             end
 
-
-                user_info(@name_input, @age_input,@weight_input,@password_input)
+                @user=user_info(@name_input, @age_input,@weight_input,@password_input)
                 puts "Welcome to our food log app please continue by logging in"
                 user_log_in
         end
@@ -129,7 +129,7 @@ class CommandLineInterface
                         q.messages[:valid?]="Ivalid number input"
                     end
                food= food_input(@food_name,@calories_input,@carbs_input,@fat_input)
-                user_options            
+                user_options           
         end
 
         def food_nutrition_info(food,user)
@@ -139,5 +139,101 @@ class CommandLineInterface
             nutrition=food_nutrition=food_nutrition_info(@food,@user)
             nutrition
         end
+        def exit 
+            puts"-------------------------------------------------------"
+                 puts"thank you for signing in #{@name_input}, sign in again soon!  "
+            puts"-------------------------------------------------------"
+
+        end
+        def user_options
+            prompt = TTY::Prompt.new
+            @user_option= prompt.select(" Choose next option")do |option|
+                option.choice 'Make another entry'
+                option.choice 'Show my history'
+                option.choice 'Update account'
+                option.choice 'Exit'
+        end
+            user=@user
+                if @user_option=='Show my history'
+                    food_log_history(user)
+                elsif @user_option=="Make another entry"
+                    create_food
+                    elsif @user_option =='Update account'
+                        update_account(user)
+                    elsif @user_option=="Exit"
+                        exit
+
+            end
+        end
+
+       
+       
+
+        def update_account(user)
+            prompt = TTY::Prompt.new
+            @update_option= prompt.select("Choose what to update")do |option|
+            option.choice 'password'
+            option.choice 'weight'
+           
+            end
+                if @update_option== 'password'
+                    update_password(user)
+                elsif @update_option=='weight'
+                    update_weight(user)
+                end
+        end
+
+        def update_password(user)#has to update
+                
+            prompt = TTY::Prompt.new
+
+            new_password_input= prompt.ask("Enter new password:") do |q|
+                q.required true
+                q.validate /^[a-zA-Z0-9_.-]*$/
+                q.messages[:valid?]="Must not contain any Symbols"
+              end
+             @password_input=new_password_input
+             puts "***password updated please sign in again***"
+             user_log_in
+
+        end
+
+        def update_weight(user)#has to update
+            prompt = TTY::Prompt.new
+
+            new_weight_input= prompt.ask("Update you weight:") do |q|
+                q.required true
+                q.validate /^(0|[1-9]\d*)(\.\d+)?$/
+                q.messages[:valid?]="Must be a number"
+              end
+             @weight_input=new_weight_input
+             puts "***Weight updated, choose another option***"
+             user_options
+            
+        
+        end
+       
+       
+      
+
+        def food_log_history(user)#shows history of specofic person
+           # user=@user
+            history = Food.all.map {|user|user}#_id==user.id}
+            history.map do |info|
+                 puts "This #{info.food_name}"
+                 puts"contains #{info.calories} calories, #{info.carbs} grams of carbs and #{info.fat} grams of fat"
+            
+            end
+            
+            user_options
+        end
+        #total up calorie count
+       #find a way to destroy entries after user exits
+    #    def run_application
+    #     user=@user_instance
+    #    # signin_or_sign_up
+    #    our_nutrition_facts
+    #    end
+       
             
 end
--- run.rb
+++ run.rb
@@ -1,6 +1,5 @@
 require_relative './config/environment'
 cli=CommandLineInterface.new
-
 cli.signin_or_sign_up
 # run_application
 
